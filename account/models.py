7eaa5a22f95bdd9ae486b4e1345fd11c9a34316e
Stefán Laxdal <stebbilax@gmail.com>
2021-04-30 20:53:31 +0000

    Added Account, PaymentInfo and SearchHistoryEntry Models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
+    photo = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class PaymentInfo(models.Model):
+    account = models.OneToOneField(Account, on_delete=models.CASCADE)
+    cvc = models.IntegerField()
+    expiration_date = models.DateField()
+    street_name = models.CharField(max_length=300)
+    house_number = models.IntegerField()
+    city = models.CharField(max_length=300)
+    postal_code = models.CharField(max_length=300)
+    name_of_cardholder = models.CharField(max_length=400)
+    card_number = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.card_number
+
+
+class SearchHistoryEntry(models.Model):
+    account = models.OneToOneField(Account, on_delete=models.CASCADE)
+    url = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.url
+        
--- ship_o_cereal/settings.py
+++ ship_o_cereal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Directory for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
+
+# Directory for media uploads
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
