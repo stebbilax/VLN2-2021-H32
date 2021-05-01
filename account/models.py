cc595024353d21f0a115e2b94827ca04abe35a43
Stefán Laxdal <stebbilax@gmail.com>
2021-05-01 10:05:02 +0000

    Added doc strings for Account, PaymentInfo and SearchHistoryEntry models

--- account/models.py
+++ account/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Account(models.Model):
+    """
+    Account Model
+
+    Attributes:
+         user       - One to one relationship with the user model
+         first_name - String field
+         last_name  - String field
+         photo      - Url to the account photo
+     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=300)
     last_name = models.CharField(max_length=300)
@@ -14,6 +22,20 @@ class Account(models.Model):
 
 
 class PaymentInfo(models.Model):
+    """
+    Payment Info Model
+
+    Attributes:
+        account             - One to one relationship with the Account model
+        cvc                 - Integer field
+        expiration_date     - Date field
+        street_name         - String field
+        house_number        - Integer field
+        city                - String field
+        postal_code         - String field
+        name_of_cardholder  - String field
+        card_number         - String field
+    """
     account = models.OneToOneField(Account, on_delete=models.CASCADE)
     cvc = models.IntegerField()
     expiration_date = models.DateField()
@@ -29,9 +51,19 @@ class PaymentInfo(models.Model):
 
 
 class SearchHistoryEntry(models.Model):
+    """
+    Search History Entry Model
+
+    - Represents individual searches made by the user
+
+    Attributes:
+        account - One to one relationship with the Account model
+        url     - String field
+    """
     account = models.OneToOneField(Account, on_delete=models.CASCADE)
     url = models.CharField(max_length=500)
 
     def __str__(self):
         return self.url
         
+
--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -1,10 +1,11 @@
+import datetime
+
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser, User
 
 from account.models import Account, PaymentInfo, SearchHistoryEntry
 
 
-# Create your tests here.
 class AccountTestCase(TestCase):
     """ Testing basic account creation """
 
@@ -25,3 +26,36 @@ class AccountTestCase(TestCase):
         account = Account.objects.get(id=self.account.id)
         self.assertEqual(account.user.username, 'bob')
         self.assertEqual(account.user.email, 'bob@bob.com')
+
+
+class PaymentInfoTestCase(TestCase):
+    """ Testing payment info creation"""
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='bob', email='bob@bob.com', password='qwerty'
+        )
+        self.account = Account.objects.create(user=self.user, first_name='Bob', last_name='Builder')
+        self.payment_info = PaymentInfo.objects.create(
+            account=self.account,
+            cvc=223,
+            expiration_date=datetime.datetime(2022, 5, 17),
+            street_name='bobstreet',
+            house_number=12,
+            city='Reykjavik',
+            postal_code=101,
+            name_of_cardholder='Bob Builder',
+            card_number='5535-1234-9876-0001'
+        )
+
+    def test_access_through_account(self):
+        """ Testing accounts access of payment info """
+        self.assertEqual(self.account.paymentinfo.cvc, 223)
+        self.assertEqual(self.account.paymentinfo.street_name, 'bobstreet')
+        self.assertEqual(self.account.paymentinfo.house_number, 12)
+        self.assertEqual(self.account.paymentinfo.city, 'Reykjavik')
+        self.assertEqual(self.account.paymentinfo.card_number, '5535-1234-9876-0001')
+
+
+
