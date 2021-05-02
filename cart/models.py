c900789d64143ea43e084a56a3ef72b8f561c05c
Stefán Laxdal <stebbilax@gmail.com>
2021-05-02 09:44:05 +0000

    Added Cart and CartItem models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from user.models import Product
+
+
+class Cart(models.Model):
+    """
+    Cart Model is a layer of abstraction between the user and his select items
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    """
+    CartItem Model links one product to one cart, keeping track of the quantity
+    """
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
