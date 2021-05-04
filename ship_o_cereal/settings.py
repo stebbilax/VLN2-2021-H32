e4ca0b1d8814bb7526233fdb74eb979587ec3eff
hildurgudrun <hildurgudrun@gmail.com>
2021-05-04 12:09:09 +0000

    navbar

--- ship_o_cereal/settings.py
+++ ship_o_cereal/settings.py
@@ -127,16 +127,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Directory for static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
 
 # Directory for media uploads
 MEDIA_URL = '/media/'
