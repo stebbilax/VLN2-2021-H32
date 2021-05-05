844d57ef1d6c7abc51fb97ca0c0729e70e626f2a
kingamaris <54936816+kingamaris@users.noreply.github.com>
2021-05-05 11:23:37 +0000

    Order url and views

--- order/views.py
+++ order/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from django import forms
 
 
 # Create your views here.
 def index(request):
     return render(request, 'order/index.html')
+
+
+def place_order(request):
+    return HttpResponse('place order')
+
+
+def send_confirmation_email(request):
+    return HttpResponse('send confirmation email')
+
+
+def save_payment_info(request):
+    return HttpResponse('save payment info')
+
+
+def checkout(request):
+    return HttpResponse('checkout')
+
