21f7756980dfda09c0592ebce9818078e547549e
hildurgudrun <hildurgudrun@gmail.com>
2021-05-10 14:08:43 +0000

    Page not found

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from .views import (index, contact_us_page, about_us_page, products_page, get_product_data,
-                    get_keywords, product_page, four_page)
+                    get_keywords, product_page, four_page, page_not_found)
 
 urlpatterns = [
     path('', index, name='home'),
     path('contact/', contact_us_page, name='contact'),
     path('about/', about_us_page, name='about'),
     path('404', four_page, name='404'),
+    path('notfound', page_not_found, name='notfound'),
     path('products/<str:category>/', products_page, name='products'),
     path('product/<int:id>/', product_page, name='product'),
     path('get_product_data/<str:category>/', get_product_data, name='get_product_data'),
